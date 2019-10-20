f9262aa0b6259c52881350fce714f198c23ce9de
siljeheldalmonsen <silje24@yahoo.com>
2019-10-20 21:08:36 +0200

    first commit

---
+++ level1/question1/script.js
@@ -0,0 +1,8 @@
+//Click event
+var myBtn = document.querySelector('button');
+
+function myAlert () {
+     alert("You will now proceed to checkout");
+}
+
+myBtn.addEventListener('click', myAlert);
---
+++ level1/question2/script.js
@@ -0,0 +1,24 @@
+// Using promises to make the API call.
+//Loop through the result using a for loop.
+//Inside the for loop, use innerHTML to create the cards.
+document.getElementById('getJson').addEventListener
+('click', getJson);
+
+function getJson() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = "<h1>CARDS</h1>";
+    data.forEach(function(card) {
+      output += `
+        <div class ="info">
+          <div><h3> userID: ${card.userId}</h3></div>
+          <div><h3> ID: ${card.id}</h3></div>
+          <div><h3> Title: ${card.title}</h3></div>
+          <div><h3> Completed: ${card.completed}<h3></div>
+        </div>
+      `;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+}
